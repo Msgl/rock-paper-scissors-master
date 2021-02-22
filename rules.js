96c9bd5c358c2401b6ddeb8abaf10e833436d832
Msgl <mariasglynou@gmail.com>
2021-02-22 17:16:36 +0200

    Rules popup complete

--- rules.js
+++ rules.js
@@ -1,15 +1,18 @@
 // Our buttons
 const rulesBtn = document.querySelector(".rules__button");
-const xBtn = document.querySelector(".box__rules__title-btn");
+const closeBtn = document.querySelector(".box__rules__title-btn");
+const wrapper = document.querySelector(".wrapper");;
 
 // The div we will open and close
 const boxRules = document.querySelector(".box__rules");
 
 
 function openRules() {
-
+    boxRules.classList.toggle('active');
+    wrapper.classList.toggle('darken');
 }
 
 function closeRules() {
-
+    boxRules.classList.remove('active');
+    wrapper.classList.toggle('darken');
 }
